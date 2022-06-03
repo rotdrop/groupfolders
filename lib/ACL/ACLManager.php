aca2d189bf5565912a33ab6ecf7841efffc2e06c
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-06-03 12:46:30 +0200

    ACLManager: make sure that the cache also holds empty ACL lists.
    Rationale: otherwise each call to getRules() will result in a cache-miss
    and fall-back to a data-base query, just to determine that there or no
    rules.

--- lib/ACL/ACLManager.php
+++ lib/ACL/ACLManager.php
@@ -60,6 +60,7 @@ class ACLManager {
 		$nonCachedPaths = array_filter($paths, fn (string $path): bool => !isset($rules[$path]));
 
 		if (!empty($nonCachedPaths)) {
+			$rules = array_merge($rules, array_fill_keys($nonCachedPaths, []));
 			$newRules = $this->ruleManager->getRulesForFilesByPath($this->user, $this->getRootStorageId(), $nonCachedPaths);
 			foreach ($newRules as $path => $rulesForPath) {
 				if ($cache) {
